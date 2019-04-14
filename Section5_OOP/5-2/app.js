cfd9db7fdf5bf82bdcd6f872c420208ac3ae2e53
Gyeongju Andy Ko <gyeongju.ko5@gmail.com>
2019-04-14 01:14:40 -0400

    5-3 updated

--- Section5_OOP/5-2/app.js
+++ Section5_OOP/5-2/app.js
@@ -31,4 +31,24 @@ const getSum1 = function(x, y) {
 
 const getSum2 = new Function("x", "y", "return 1 + 1");
 
-console.log(getSum2(1, 1));
+// console.log(getSum2(1, 1));
+
+// Object
+const john1 = {
+  name: "John"
+};
+const john2 = new Object({ name: "John" });
+
+// console.log(john1);
+// console.log(john2);
+
+// Arrays
+const arr1 = [1, 2, 3];
+const arr2 = new Array(1, 2, 3, 4);
+
+// Regular Expressions
+const re1 = /\w+/;
+const re2 = new RegExp("w+");
+
+console.log(re1);
+console.log(re2);
